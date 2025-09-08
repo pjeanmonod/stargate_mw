362b68d48cd079ea87f13e7fc75fddd8831830ef
Pjeanmonod <peter_jeanmonod@hotmail.com>
2025-09-08 16:10:20 +0000

    bertie big potatoes

--- middleware/apps/cloud01/input_handler.py
+++ middleware/apps/cloud01/input_handler.py
@@ -5,17 +5,17 @@ def format_awx_request(build_request):
     pprint (build_request)
 
     job_vars = {
-        'CorePriCIDR': build_request.get['CorePriCIDR'],
-        'CoreSecCIDR': build_request.get['CoreSecCIDR'],
-        'CoreprivateSubnetsPerAZ': build_request.get['CoreprivateSubnetsPerAZ'],
-        'CorepublicSubnetsPerAZ': build_request.get['CorepublicSubnetsPerAZ'],
-        'coreIgw': build_request.get['CoreIgw'],
-        'EdgePriCIDR': build_request.get['EdgePriCIDR'],
-        'EdgeSecCIDR': build_request.get['EdgeSecCIDR'],
-        'EdgeprivateSubnetsPerAZ': build_request.get['EdgeprivateSubnetsPerAZ'],
-        'EdgepublicSubnetsPerAZ': build_request.get['EdgepublicSubnetsPerAZ'],
-        #'EdgeIgw' : build_request.get['EdgeIgw'],
-        'azsperVPC': build_request.get['azsperVPC'],
+        'CorePriCIDR': build_request.get('CorePriCIDR'),
+        'CoreSecCIDR': build_request.get('CoreSecCIDR'),
+        'CoreprivateSubnetsPerAZ': build_request.get('CoreprivateSubnetsPerAZ'),
+        'CorepublicSubnetsPerAZ': build_request.get('CorepublicSubnetsPerAZ'),
+        'coreIgw': build_request.get('CoreIgw'),
+        'EdgePriCIDR': build_request.get('EdgePriCIDR'),
+        'EdgeSecCIDR': build_request.get('EdgeSecCIDR'),
+        'EdgeprivateSubnetsPerAZ': build_request.get('EdgeprivateSubnetsPerAZ'),
+        'EdgepublicSubnetsPerAZ': build_request.get('EdgepublicSubnetsPerAZ'),
+        'EdgeIgw' : build_request.get['EdgeIgw'],
+        'azsperVPC': build_request.get('azsperVPC'),
         "region": build_request.get("region"),
         "tags": build_request.get("tags"),   
     }

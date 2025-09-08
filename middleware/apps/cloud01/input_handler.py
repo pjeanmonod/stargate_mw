ece84eaebdf16fabd9b1c5c24c2ca35b57a7a828
Pjeanmonod <peter_jeanmonod@hotmail.com>
2025-09-08 15:50:45 +0000

    bunga bunga

--- middleware/apps/cloud01/input_handler.py
+++ middleware/apps/cloud01/input_handler.py
@@ -5,8 +5,19 @@ def format_awx_request(build_request):
     pprint (build_request)
 
     job_vars = {
-        "EdgePriCIDR": build_request.get("EdgePriCIDR"),
-        "ipv4CIDR": build_request.get("ipv4CIDR"),
+        'CorePriCIDR': build_request.get['CorePriCIDR'],
+        'CoreSecCIDR': build_request.get['CoreSecCIDR'],
+        'CoreprivateSubnetsPerAZ': build_request.get['CoreprivateSubnetsPerAZ'],
+        'CorepublicSubnetsPerAZ': build_request.get['CorepublicSubnetsPerAZ'],
+        'coreIgw': build_request.get['CoreIgw'],
+
+        'EdgePriCIDR': build_request.get['EdgePriCIDR'],
+        'EdgeSecCIDR': build_request.get['EdgeSecCIDR'],
+        'EdgeprivateSubnetsPerAZ': build_request.get['EdgeprivateSubnetsPerAZ'],
+        'EdgepublicSubnetsPerAZ': build_request.get['EdgepublicSubnetsPerAZ'],
+        'EdgeIgw' : build_request.get['EdgeIgw'],
+
+        'azsperVPC': build_request.get['azsperVPC'],
         "region": build_request.get("region"),
         "tags": build_request.get("tags"),   
     }

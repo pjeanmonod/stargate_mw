d6005092cd64652c892fc204d535c04277c7b4e4
Pjeanmonod <peter_jeanmonod@hotmail.com>
2025-10-06 22:07:23 +0000

    admin.py

--- middleware/apps/cloud01/admin.py
+++ middleware/apps/cloud01/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import BuildRequest
+from .models import BuildRequest, InfraOutput
 
-# Register your models here.
-
+# Register your models here
 admin.site.register(BuildRequest)
+admin.site.register(InfraOutput)

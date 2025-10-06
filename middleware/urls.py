de46956b1a6f586c5a477fcfcfa3fae42a9aca06
Pjeanmonod <peter_jeanmonod@hotmail.com>
2025-10-06 22:09:42 +0000

    admin url

--- middleware/urls.py
+++ middleware/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-
+    path('admin/', admin.site.urls),
     path('api/cloud01/', include('middleware.apps.cloud01.urls')),
 
 ]

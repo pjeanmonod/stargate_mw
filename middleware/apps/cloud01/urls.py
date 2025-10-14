a51481b35d344d61ebe5c33fc9cb6fd39d9e3355
Pjeanmonod <peter_jeanmonod@hotmail.com>
2025-10-14 12:54:23 +0000

    badger5

--- middleware/apps/cloud01/urls.py
+++ middleware/apps/cloud01/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from . import views
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import CustomTokenObtainPairView, InfraOutputViewSet, TerraformPlanViewSet, TerraformCallbackView
+from .views import CustomTokenObtainPairView, InfraOutputViewSet, TerraformPlanViewSet
 
 router = routers.DefaultRouter()
 # router.register(r'buildrequest', views.BuildRequestViewSet, basename='BuildRequest')
@@ -15,7 +15,6 @@ urlpatterns = [
     path('configure', views.configure.as_view()),
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path("terraform/callback/", TerraformCallbackView.as_view(), name="terraform-callback"),
     path("terraform/approve/<int:job_id>/", views.approve_terraform_plan, name="terraform-approve"),
 ]
 

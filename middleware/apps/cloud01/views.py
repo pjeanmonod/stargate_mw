fdb1d6689311c8141df8798d9752ffe985f0d708
Pjeanmonod <peter_jeanmonod@hotmail.com>
2025-10-15 20:25:24 +0000

    badger

--- middleware/apps/cloud01/views.py
+++ middleware/apps/cloud01/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.decorators import action
 import traceback
 from pprint import pprint
 

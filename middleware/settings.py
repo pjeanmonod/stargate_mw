8cf564abd05ce6488e4abd369f8840a10ea37f11
Pjeanmonod <peter_jeanmonod@hotmail.com>
2025-10-16 08:50:56 +0000

    woops

--- middleware/settings.py
+++ middleware/settings.py
@@ -33,7 +33,6 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
-    'middleware.apps.cloud01',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

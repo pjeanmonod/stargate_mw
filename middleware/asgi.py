d036fbcc2d4a4afa7e52042e8458ec8b16aebc68
Pjeanmonod <peter_jeanmonod@hotmail.com>
2025-11-16 15:48:55 +0000

    BERTIE BADGER

--- middleware/asgi.py
+++ middleware/asgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import cloud01.routing
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'middleware.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            cloud01.routing.websocket_urlpatterns
+        )
+    ),
+})
